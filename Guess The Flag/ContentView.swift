0d3ad4838605b01b76d7ae532efe19830f404e5a
raster-gen <rasterroot@gmail.com>
2022-11-02 23:19:04 +0000

    added functionality correct/wrong answers and alert for this one

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
 
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
 
     
     var body: some View {
@@ -27,15 +29,37 @@ struct ContentView: View {
                 
                 ForEach(0..<3){ number in
                     Button{
-                        
+                        //flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
                     
                 }
+                
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is >>>")
+        }
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
