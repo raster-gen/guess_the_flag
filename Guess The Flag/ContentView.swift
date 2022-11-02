45752723a101f496d61030800b5eb92d018037c4
raster-gen <rasterroot@gmail.com>
2022-11-02 22:47:53 +0000

    created list of countries and 3 btn flags

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                
+                ForEach(0..<3){ number in
+                    Button{
+                        
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                    
+                }
+            }
+        }
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//        ContentView()
+//            .preferredColorScheme(.dark)
     }
 }
